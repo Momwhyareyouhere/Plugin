5b118800eadd917a81a8a313ed079301a0a32867
Momwhyareyouhere <151050626+Momwhyareyouhere@users.noreply.github.com>
2024-02-27 19:53:16 +0100

    Update example.plugin.js

--- example.plugin.js
+++ example.plugin.js
@@ -1,17 +1,41 @@
 /**
- * @name YourPlugin
+ * @name BackgroundColorChanger
  * @author YourName
- * @description Describe the basic functions. Maybe a support server link.
+ * @description A BetterDiscord plugin that changes the background color to white.
  * @version 0.0.1
  */
 
-module.exports = class YourPlugin {
+module.exports = class BackgroundColorChanger {
     start() {
-      // Called when the plugin is activated (including after reloads)
-      BdApi.alert("Hello World!", "This is my first plugin!");
-    } 
+        // Called when the plugin is activated (including after reloads)
+        this.addButton();
+    }
 
     stop() {
-      // Called when the plugin is deactivated
+        // Called when the plugin is deactivated
+        this.removeButton();
     }
-}
+
+    addButton() {
+        const serverList = document.querySelector('.guilds-1SWlCJ');
+        if (!serverList) return;
+
+        const button = document.createElement('button');
+        button.textContent = 'Change Background Color';
+        button.addEventListener('click', this.handleButtonClick.bind(this));
+
+        serverList.appendChild(button);
+    }
+
+    removeButton() {
+        const button = document.querySelector('.guilds-1SWlCJ button:contains("Change Background Color")');
+        if (button) {
+            button.remove();
+        }
+    }
+
+    handleButtonClick() {
+        document.body.style.backgroundColor = 'white';
+        BdApi.alert('Background Color Changed', 'The background color has been changed to white.');
+    }
+};
